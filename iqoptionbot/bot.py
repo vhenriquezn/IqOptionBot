7e4e1515171bb0ce1d0e0a96f7d9bd7cb90909fc
vhenriquezn <32468931+vhenriquezn@users.noreply.github.com>
2025-07-22 16:14:21 -0400

    Update bot.py

--- iqoptionbot/bot.py
+++ iqoptionbot/bot.py
@@ -2,13 +2,13 @@ import pandas as pd
 import time
 from datetime import datetime
 from iqoptionapi.stable_api import IQ_Option
-from utils import borrar_lineas, cargar_config, get_estrategia, mostrar_tabla, validar_entrada
+from iqoptionbot import utils
 
 class BotModular:
     def __init__(self, login_path="login.txt", config_path="config.txt"):
 
-        self.config = cargar_config(config_path)
-        login = cargar_config(login_path)
+        self.config = utils.cargar_config(config_path)
+        login = utils.cargar_config(login_path)
 
         self.api = IQ_Option(email=login.get("email"), password=login.get("password"))
 
@@ -37,12 +37,11 @@ class BotModular:
         print("✅ Conectado correctamente.\n")
 
     def set_account(self):
-        lineas_clr = 1
         opcion = input(f">> Estás en cuenta {self.api.get_balance_mode()}. ¿Deseas cambiar? (y/n): ").strip().lower()
 
         if opcion == 'y':
             balances = self.api.get_balances()
-            borrar_lineas(1)
+            utils.borrar_lineas(1)
             print("📊 BALANCES DISPONIBLES:\n")
             opciones_disponibles = []
             for i, balance in enumerate(balances.get("msg", []), start=1):
@@ -60,27 +59,28 @@ class BotModular:
                 }.get(tipo, f"Desconocido ({tipo})")
                 print(f"   [{i}] {tipo_str:<8}: {monto:>8.2f} {moneda}")
                 opciones_disponibles.append(tipo_str)
-                lineas_clr += 1
-
+                
             while True:
                 eleccion = input("\n>> Ingresa el número de la cuenta: ").strip()
                 if eleccion.isdigit() and 1 <= int(eleccion) <= len(opciones_disponibles):
                     seleccion = opciones_disponibles[int(eleccion) - 1]
                     self.api.change_balance(seleccion)
-                    lineas_clr += 3
                     break
                 else:
                     print("❌ Opción inválida. Por favor, elige 1 o 2.")
-                    lineas_clr += 2
 
-        borrar_lineas(lineas_clr)
-        print(f"{"Bienvenido/a:":<17}{self.email}\n")
-        print(f"{"Tipo de cuenta:":<17}{self.api.get_balance_mode()} ${self.api.get_balance()} {self.api.get_currency()}")
-        print(f"{"Valor entrada:":<17}{self.entrada_actual}")
+        utils.borrar_lineas(len(opciones_disponibles)+4)
+        print(f"{"Bienvenido/a":<17}:{self.email}\n")
+        print(f"{"Tipo de cuenta":<17}:{self.api.get_balance_mode()} ${self.api.get_balance()} {self.api.get_currency()}")
+        print(f"{"Valor entrada":<17}:{self.entrada_actual}")
         if self.use_stop_win:
-            print(f"{"Stop Win:":<17}{stop_win}")
+            print(f"{"Stop Win":<17}:{stop_win}")
         if self.use_stop_loss:
-            print(f"{"Stop Loss:":<17}-{stop_loss}\n")
+            print(f"{"Stop Loss":<17}:-{stop_loss}")
+        if self.use_mg:
+            print(f"{"Niveles MG":<17}:{self.nivel_mg}")
+        if self.use_media_movil:
+            print(f"{"Periodo":<17}:{self.periodo_medias}")
 
     def trading_loop(self):
         self.actualizar_entrada()
@@ -109,10 +109,23 @@ class BotModular:
 
                 senal = strategy(df)
                 if senal:
-                    precio_entrada = df.iloc[-1]["Open"]
-                    if self.validar_precio_favorable(senal, precio_entrada):
+                    precio_entrada = df.iloc[-1]["open"]
+                    isValida, info = self.validar_senal(senal, precio_entrada)
+                    if isValida:
                         msg = f">>🔔 Señal de {'COMPRA' if senal == 'call' else 'VENTA'} detectada, esperando resultado...⏳"
                         self.ejecutar_operacion(senal, msg, hora_op=datetime.fromtimestamp(int(time.time()) // 60 * 60).strftime('%H:%M:%S'))
+                    else:
+                         self.operaciones.append({
+                            "hora": datetime.fromtimestamp(int(time.time()) // 60 * 60).strftime('%H:%M:%S'),
+                            "paridad": self.asset,
+                            "direccion": senal.upper(),
+                            "inversion": 0,
+                            "resultado": info,
+                            "mg": None,
+                            "lucro": 0
+                        })
+
+                        ganancia_total = utils.mostrar_tabla(self.operaciones)   
                 else:
                     message = f"{datetime.fromtimestamp(int(time.time()) // 60 * 60).strftime('%H:%M:%S')} - No hay señal en esta vela, "
         except KeyboardInterrupt:
@@ -121,6 +134,9 @@ class BotModular:
             print(f"❌ Error inesperado: {e}")
         #finally:
             #guardar_operaciones_excel(operaciones)
+    
+    def actualizar_activo(self, nuevo_activo):
+        self.asset = nuevo_activo
 
     def actualizar_entrada(self):
         balance = self.api.get_balance()
@@ -132,12 +148,11 @@ class BotModular:
 
     def seleccionar_activo_abierto(self):
         print("🕵️‍♂️ Iniciando búsqueda de activos abiertos en la plataforma...")
-        #print(self.api.get_all_ACTIVES_OPCODE())
         activos = self.api.get_all_open_time()
         filtered_assets = []
 
         if "binary" not in activos:
-                borrar_lineas(1)
+                utils.borrar_lineas(1)
                 print("❌ No hay sección 'binary' en la respuesta.")
                 return []
             
@@ -147,22 +162,24 @@ class BotModular:
                     filtered_assets.append((asset, clean_asset))
 
         if not filtered_assets:
-                borrar_lineas(1)
+                utils.borrar_lineas(1)
                 print("⚠️ No hay activos binarios abiertos.")
                 return []
 
         # Mostrar activos en 4 columnas
-        borrar_lineas(1)
+        utils.borrar_lineas(1)
         print("📈 Activos binarios abiertos disponibles:\n")
         col_width = 30  # Ajusta el ancho por columna si lo deseas
 
         for i, asset in enumerate(filtered_assets, start=1):
             text = f"[{i:^2}] {asset[1]}"
-            print(text.ljust(col_width), end='')
             if i % 4 == 0:
-                print()  # Salto de línea cada 4 columnas
+                text += "\n"
+            else:
+                text.ljust(col_width)  # Salto de línea cada 4 columnas
         if len(filtered_assets) % 4 != 0:
-            print()  # Salto final si no termina justo en múltiplo de 4
+            text += "\n"  # Salto final si no termina justo en múltiplo de 4
+        print(text)  # Salto final si no termina justo en múltiplo de 4
 
         # Selección del usuario
         while True:
@@ -170,7 +187,7 @@ class BotModular:
                 choice = int(input("\nSeleccione un activo (número): "))
                 if 1 <= choice <= len(filtered_assets):
                     selected = filtered_assets[choice - 1]
-                    borrar_lineas((len(filtered_assets) // 4 + 4))  # borra líneas según filas + extras
+                    utils.borrar_lineas((len(filtered_assets) // 4 + 4))  # borra líneas según filas + extras
                     return selected[1]
                 else:
                     print("Número fuera de rango. Intenta de nuevo.")
@@ -182,7 +199,7 @@ class BotModular:
         tiempo_espera = (60 - segundos + espera - 1) if segundos > espera else (espera - 1 - segundos)
         while tiempo_espera > 0:
             tiempo_espera -= 1
-            borrar_lineas(1)
+            utils.borrar_lineas(1)
             print(f">>{message}⏳ esperando próxima vela en {tiempo_espera} segundos...")
             time.sleep(1)
 
@@ -190,45 +207,39 @@ class BotModular:
         candles = self.api.get_candles(asset, offset, period, end_time)
         df = pd.DataFrame(candles)
         df['Date'] = pd.to_datetime(df['from'], unit='s', utc=True).dt.tz_convert("America/Santiago")
-        df = df[['Date', 'open', 'max', 'min', 'close']]
-        df.columns = ['Date', 'Open', 'High', 'Low', 'Close']
-        df = df.sort_values('Date').reset_index(drop=True)
+        df = df[['date', 'open', 'max', 'min', 'close']]
+        df.columns = ['date', 'open', 'high', 'low', 'close']
+        df = df.sort_values('date').reset_index(drop=True)
         return df            
 
-    def validar_precio_favorable(self, signal, precio_entrada, duracion=30):
+    def validar_senal(self, signal, precio_entrada, duracion=30):
         df = self.obtener_candles(self.asset, int(time.time()), offset=60, period=60)
         if len(df) < 15:
-            borrar_lineas(1)
-            print("⚠️ No hay suficientes velas para calcular SMA.")
-            return
+            return False, "⚠️ No hay suficientes velas para calcular SMA"
         
         if self.use_media_movil:
             if not validar_entrada(df, signal, self.periodo_medias):
-                borrar_lineas(1)
-                print("❌ Condición de SMA no cumplida. No se ejecuta operación.")
-                return
+                return False, "❌ Condición de SMA no cumplida"
 
         if not precio_entrada:
-            return True
+            return True, None
         tiempo_limite = time.time() + duracion
             
         while time.time() < tiempo_limite:
             vela = self.api.get_candles(self.asset, 60, 1, int(time.time()))
             if not vela:
-                borrar_lineas(1)
+                utils.borrar_lineas(1)
                 print("❌ Error al obtener precio actual.")
                 time.sleep(1)
                 continue
             precio_actual = vela[0]["close"]
             if (signal == "call" and precio_actual <= precio_entrada) or (signal == "put" and precio_actual >= precio_entrada):
-                return True
-            borrar_lineas(1)    
+                return True, None
+            utils.borrar_lineas(1)    
             print(f"Esperando mejor precio ({signal.upper()}): actual={precio_actual:.5f}, apertura={precio_entrada:.5f}...")
             time.sleep(0.5)
         borrar_lineas(1)
-        print(f"⚠️ Cancelada operación {signal.upper()} por no alcanzar precio favorable en {duracion}s.")
-        time.sleep(1)
-        return False
+        return False, f"⚠️ No alcanzó precio favorable en {duracion}s"
 
     def ejecutar_operacion(self, signal, message_check, hora_op=None):
         nivel = 0
@@ -270,7 +281,7 @@ class BotModular:
             else:
                 nivel += 1
                 entrada = round(entrada * self.factor_mg, 0)
-                message_check = f">>🔁 Nivel MG {nivel} activado, nueva entrada: {entrada}, esperando resultado en "
+                message_check = f">>🔁 Nivel MG {nivel} activado, nueva entrada: {entrada}, esperando resultado..."
                 time.sleep(0.5)
 
         self.operaciones.append({
@@ -283,7 +294,7 @@ class BotModular:
             "lucro": total_profit
         })
 
-        ganancia_total = mostrar_tabla(self.operaciones, 5)
+        ganancia_total = mostrar_tabla(self.operaciones)
 
         if self.use_stop_win and ganancia_total >= self.stop_win:
             print("🎯 Stop Win alcanzado. Deteniendo operaciones.")
